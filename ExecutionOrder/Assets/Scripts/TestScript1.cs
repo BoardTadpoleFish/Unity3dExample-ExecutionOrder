a59d56e569198e312267b87318362914b277c54b
litsungyi <litsungyi@gmail.com>
2018-07-24 04:13:46 +0800

    Update Test1

---
+++ ExecutionOrder/Assets/Scripts/TestScript1.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace BoardTadpoleFish
+{
+    public class TestScript1 : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log(string.Format("{0} - Awake", name));
+        }
+
+        private void Start()
+        {
+            Debug.Log(string.Format("{0} - Start", name));
+        }
+
+        private void OnEnable()
+        {
+            Debug.Log(string.Format("{0} - OnEnable", name));
+        }
+
+        private void OnDisable()
+        {
+            Debug.Log(string.Format("{0} - OnDisable", name));
+        }
+
+        private void OnDestroy()
+        {
+            Debug.Log(string.Format("{0} - OnDestroy", name));
+        }
+
+        //private void Update()
+        //{
+        //    Debug.Log(string.Format("{0} - Update", name));
+        //}
+    }
+}
---
+++ ExecutionOrder/Assets/Scripts/TestScript1Root.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace BoardTadpoleFish
+{
+    public class TestScript1Root : MonoBehaviour
+    {
+        [SerializeField] private GameObject object1;
+        [SerializeField] private GameObject object2;
+
+        private void Awake()
+        {
+            StartCoroutine(Process());
+        }
+
+        private IEnumerator Process()
+        {
+            yield return ProcessEndOfFrame();
+
+            Debug.LogWarning("EnableObject2");
+            object2.SetActive(true);
+
+            yield return ProcessEndOfFrame();
+
+            Debug.LogWarning("DisableObject1");
+            object1.SetActive(false);
+
+            yield return ProcessEndOfFrame();
+
+            Debug.LogWarning("DestroyObject1");
+            Object.Destroy(object1);
+            object1 = null;
+
+            yield return ProcessEndOfFrame();
+
+            Debug.LogWarning("DestroyObject2");
+            Object.Destroy(object2);
+            object2 = null;
+
+            yield return ProcessEndOfFrame();
+        }
+
+        private IEnumerator ProcessEndOfFrame()
+        {
+            Debug.LogWarning("<===");
+            yield return new WaitForEndOfFrame();
+            Debug.LogWarning("===>");
+        }
+    }
+}
