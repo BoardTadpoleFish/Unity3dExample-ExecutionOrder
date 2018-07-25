b76aaa1b77cc023b876f0f2eed4d475f2f0e1c72
litsungyi <litsungyi@gmail.com>
2018-07-25 09:09:24 +0800

    Refactoring, add log message

--- ExecutionOrder/Assets/Scripts/TestScript1Root.cs
+++ ExecutionOrder/Assets/Scripts/TestScript1Root.cs
@@ -17,24 +17,28 @@ namespace BoardTadpoleFish
         {
             yield return ProcessWait();
 
-            Debug.LogWarning("EnableObject2");
+            Debug.LogWarning("# EnableObject2");
             object2.SetActive(true);
+            Debug.LogWarning("# After EnableObject2");
 
             yield return ProcessWait();
 
-            Debug.LogWarning("DisableObject1");
+            Debug.LogWarning("# DisableObject1-1");
             object1.SetActive(false);
+            Debug.LogWarning("# After DisableObject1-1");
 
             yield return ProcessWait();
 
-            Debug.LogWarning("DestroyObject1");
+            Debug.LogWarning("# DestroyObject1");
             Object.Destroy(object1);
+            Debug.LogWarning("# After DestroyObject1");
             object1 = null;
 
             yield return ProcessWait();
 
-            Debug.LogWarning("DestroyObject2");
+            Debug.LogWarning("# DestroyObject2");
             Object.Destroy(object2);
+            Debug.LogWarning("# After DestroyObject2");
             object2 = null;
 
             yield return ProcessWait();
